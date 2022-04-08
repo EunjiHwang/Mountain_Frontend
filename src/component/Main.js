6a010250b81b6e07d9717b57fcfbb13f4c9fe5c8
qkrthdus605 <qkrthdus605@gmail.com>
2022-04-08 21:20:43 +0900

    style : 불필요한 파일 삭제 및 Main 컴포넌트 배경 svg 파일 추가

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,7 +1,15 @@
+import React from 'react';
+// import styled from 'styled-component';
+
+// const Button = styled.button`
+//   width: 100px;
+//   height: 60px;
+// `;
+
 function Main() {
   return (
     <div>
-      <h1>메인화면</h1>
+      <h1>메인</h1>
     </div>
   );
 }
