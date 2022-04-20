e41a1a51eb0739e0c1b55d8362ec7ac2fcc2ed60
qkrthdus605 <qkrthdus605@gmail.com>
2022-04-21 01:08:39 +0900

    feature : 헤더 기본메뉴 구성 구현 완료

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,10 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+// 각 메뉴별 현재 없는 컴포넌트 라우터 설정 필요
+// 드롭다운 구현 필요
+
 const Wrapper = styled.div`
   width: 100%;
   height: 80px;
   background-color: white;
+  display: flex;
+  flex-direction: row;
   border-bottom: 3px solid #4C8969;
 `;
 
@@ -15,15 +20,68 @@ const HomeButton = styled.div`
   font-size: 50px;
   color: black;
   font-weight: 500;
-  margin: 5px 5px 5px 40px;
+  margin: 5px 5px 5px 70px;
 `;
 
+const MenuList = styled.div`
+  width: 500px;
+  height: 40px;
+  margin: 30px 0 0 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MenuItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 20px;
+  font-weight: 600;
+  color: black;
+`;
+
+const AuthList = styled.div`
+  width: 500px;
+  height: 40px;
+  margin: 20px 0 0 100px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+`;
+
+const AuthItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 20px;
+  font-weight: 600;
+  color: black;
+  margin-left: 40px;
+`;
+
+
 function Header() {
+
   return (
     <Wrapper>
       <Link to="/" style={{ textDecoration: 'none' }}>
         <HomeButton>燈山</HomeButton>
       </Link>
+      <MenuList>
+        <MenuItem>소개글</MenuItem>
+        <Link to="/community" style={{ textDecoration: 'none' }}>
+          <MenuItem>커뮤니티</MenuItem>
+        </Link>
+        <Link to="/map" style={{ textDecoration: 'none' }}>
+          <MenuItem>지도</MenuItem>
+        </Link>
+        <MenuItem>마이페이지</MenuItem>
+      </MenuList>
+      <AuthList>
+        <Link to="/login" style={{textDecoration: 'none'}}>
+          <AuthItem>로그인</AuthItem>
+        </Link>
+        <AuthItem>회원가입</AuthItem>
+      </AuthList>
     </Wrapper>
   );
 }
