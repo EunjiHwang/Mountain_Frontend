9c4baff5b82b79df132fa3db5d99e3c301fbf093
qkrthdus605 <qkrthdus605@gmail.com>
2022-04-21 02:34:17 +0900

    feature : Background 이미지 추가 및 텍스트 추가

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,13 +1,55 @@
 import React from 'react';
-import Header from './Header'; 
-import Footer from './Footer';
-  
+import styled from 'styled-components';
+
+const Background = styled.div`
+  top: 0;
+  left: 0;
+  position: absolute;
+  min-width: 100%;
+  height: 100vh;
+  background: url(${'../img/Background.png'});
+  background-repeat: none;
+  background-size: cover;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 40px;
+  height: 160px;
+  margin: 30px 0 0 30px;
+`;
+
+const LogoText = styled.div`
+  font-size: 40px;
+  font-weight: 600;
+  color: white;
+  display: flex;
+`;
+
+const SubContainer = styled.div`
+  align-items: center;
+  margin-top: 200px;
+`;
+const SubText = styled.div`
+  font-weight: 600;
+  color: white;
+  font-size: 28px;
+  text-align: center;
+`;
+
 function Main() {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <Background>
+      <LogoContainer>
+        <LogoText>燈山</LogoText>
+      </LogoContainer>
+      <SubContainer>
+        <SubText>등산을 좋아하는 사람들이 모여 소통하는 공간</SubText>
+        <SubText>등산 커뮤니티 서비스, 燈山</SubText>
+      </SubContainer>
+    </Background>
   );
 }
 
