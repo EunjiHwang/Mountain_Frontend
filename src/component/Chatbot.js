e56702757f397c0b7ce903cc6c6e185354878dc3
qkrthdus605 <qkrthdus605@gmail.com>
2022-05-31 18:27:39 +0900

    feature: 촬영용 질문/답변 추가

--- src/component/Chatbot.js
+++ src/component/Chatbot.js
@@ -22,10 +22,13 @@ const ChatContainer = styled.div`
   height: 470px;
   position: fixed;
   background-color: white;
+  display: flex;
+  flex-direction: column;
   right: 50px;
   bottom: 120px;
   border: 3px solid #a0e991;
   border-radius: 10px;
+  display: flex;
 `;
 
 // 챗봇 input
@@ -44,9 +47,77 @@ const ChatInput = styled.input`
   }
 `;
 
-// 내가 보내는 말풍선
+// 나
+const MyContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 300px;
+  height: 50px;
+  margin: 5px 0 0 40px;
+`;
 
-// 상대방 말풍선
+// 나를 표시하는 텍스트
+const MyTitle = styled.div`
+  width: 40px;
+  height: 40px;
+  font-weight: 600;
+  color: #4c8969;
+  border: 2px solid #4c8969;
+  border-radius: 30px;
+  text-align: center;
+  padding-top: 10px;
+  margin-top: 3px;
+`;
+
+// 내가 보내는 말풍선
+const MyMsg = styled.div`
+  width: 250px;
+  height: 35px;
+  position: relative;
+  background-color: #a0e991;
+  border-radius: 15px;
+  font-size: 12px;
+  margin: 5px 5px 0 0;
+  padding: 10px;
+  
+`;
+
+// 봇
+const BotContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  width: 300px;
+  height: 50px;
+  margin: 5px 0 0 5px;
+`;
+
+// 봇을 표시하는 텍스트
+const BotTitle = styled.div`
+  width: 40px;
+  height: 40px;
+  font-weight: 600;
+  color: white;
+  background-color: #4c8969;
+  border-radius: 30px;
+  text-align: center;
+  padding-top: 12px;
+  margin-top: 3px;
+`;
+
+// 봇이 보내는 말풍선
+const BotMsg = styled.div`
+  width: 250px;
+  height: 38px;
+  position: relative;
+  border: 2px solid #a0e991;
+  border-radius: 17px;
+  font-size: 12px;
+  margin: 5px 5px 0 5px;
+  padding: 5px;
+  line-height: 1.1;
+`;
+
 
 //챗봇 아이콘
 const IconWrap = styled.div`
@@ -72,108 +143,141 @@ const IconImg = styled.img`
 function Chatbot() {
   const [isClicked, setIsClicked] = useState(false);
 
-  const dispatch = useDispatch();
+  // 사용자 메시지, 응답
+  const [sendMsg, setSendMsg] = useState('');
+  const [resMsg, setResMsg] = useState('');
+  const [secondMsg, setSecondMsg] = useState('');
+  const [resSecondMsg, setResSecondMsg] = useState('');
 
-  useEffect(() => {
-    eventQuery('welcomeToMyWebsite')
-  },[])
+  const dispatch = useDispatch();
 
   const handleIcon = () => {
     setIsClicked(!isClicked);
   };
 
-  // 서버로 요청을 보낼 함수
-  const textQuery = async (text) => {
-    // 1. 보낸 메시지를 관리
-    let conversation = {
-      who: 'user',
-      content: {
-        text: {
-          text: text,
-        },
-      },
-    };
 
-    dispatch(saveMessage(conversation));
-
-    // 2. 챗봇이 보낸 메시지를 처리
-    const textQueryVar = {
-      text,
-    };
-    // request 보내기
-    try {
-      // request to textQuery Route
-      const response = await Axios.post(
-        'http://54.208.255.25:8080/api/dialogflow/textQuery',
-        textQueryVar
-      );
-      const content = response.data.fulfillmentMessages[0];
-
-      conversation = {
-        who: 'bot',
-        content: content,
-      };
-      console.log(conversation);
-    } catch (error) {
-      conversation = {
-        who: 'bot',
-        content: {
-          text: {
-            text: 'Error가 발생했습니다. 문제를 확인해주세요.',
-          },
-        },
-      };
-      console.log(conversation);
-    }
-  };
-
-
-
-  const eventQuery = async (event) => {
-
-
-    // 2. 챗봇이 보낸 메시지를 처리
-    const eventQueryVar = {
-      event
-    }
-    // request 보내기
-    try {
-      // request to textQuery Route
-      const response = await Axios.post('/api/dialogflow/eventQuery', eventQueryVar)
-      const content = response.data.fulfillmentMessages[0] 
-
-      let conversation = {
-        who: 'bot',
-        content: content
-      }
-      console.log(conversation);
-    } catch (error) {
-      let conversation = {
-        who: 'bot',
-        content: {
-          text: {
-            text: 'Error가 발생했습니다. 문제를 확인해주세요.'
-          }
-        }
-      }
-      console.log(conversation);
-    }
-
-  }
-
+  // 임시 촬영용 handler
   const onKeyHandler = (e) => {
-    // 엔터키 이벤트
     if (e.key === 'Enter') {
-      // 아무것도 입력하지 않았을 경우
       if (!e.target.value) {
         return alert('메시지를 입력해주세요!');
+      } else {
+        // input을 sendMsg로 set
+        if (e.target.value === '등산 준비물') {
+          setSendMsg(e.target.value);
+          setTimeout(function () {
+            setResMsg('편한 복장, 등산화, 손전등, 물, 간식, 비상약, 휴지 등을 챙기시는걸 추천드려요!');
+          }, 1500);
+        } else if (e.target.value === '등산 시간') {
+          setSecondMsg(e.target.value);
+          setTimeout(function () {
+            setResSecondMsg('하루에 산행은 8시간 이내로 하고 체력의 30%는 비축해두는게 좋아요!');
+          }, 1500);
+        } else {
+          setSendMsg(e.target.value);
+          setTimeout(function () {
+            setResMsg('지원하지 않는 답변입니다ㅠㅠ');
+          }, 1500);
+        }
       }
-      // 메시지를 입력했다면 이를 보냄
-      textQuery(e.target.value);
-      // 보낸 후에는 value를 빈 값으로
       e.target.value = '';
     }
-  };
+  }
+
+  // useEffect(() => {
+  //   eventQuery('welcomeToMyWebsite');
+  // }, []);
+
+  // 서버로 요청을 보낼 함수
+  // const textQuery = async (text) => {
+  //   // 1. 보낸 메시지를 관리
+  //   let conversation = {
+  //     who: 'user',
+  //     content: {
+  //       text: {
+  //         text: text,
+  //       },
+  //     },
+  //   };
+
+  //   dispatch(saveMessage(conversation));
+
+  //   // 2. 챗봇이 보낸 메시지를 처리
+  //   const textQueryVar = {
+  //     text,
+  //   };
+  //   // request 보내기
+  //   try {
+  //     // request to textQuery Route
+  //     const response = await Axios.post(
+  //       'http://54.208.255.25:8080/api/dialogflow/textQuery',
+  //       textQueryVar
+  //     );
+  //     const content = response.data.fulfillmentMessages[0];
+
+  //     conversation = {
+  //       who: 'bot',
+  //       content: content,
+  //     };
+  //     console.log(conversation);
+  //   } catch (error) {
+  //     conversation = {
+  //       who: 'bot',
+  //       content: {
+  //         text: {
+  //           text: 'Error가 발생했습니다. 문제를 확인해주세요.',
+  //         },
+  //       },
+  //     };
+  //     console.log(conversation);
+  //   }
+  // };
+
+  // const eventQuery = async (event) => {
+  //   // 2. 챗봇이 보낸 메시지를 처리
+  //   const eventQueryVar = {
+  //     event,
+  //   };
+  //   // request 보내기
+  //   try {
+  //     // request to textQuery Route
+  //     const response = await Axios.post(
+  //       '/api/dialogflow/eventQuery',
+  //       eventQueryVar
+  //     );
+  //     const content = response.data.fulfillmentMessages[0];
+
+  //     let conversation = {
+  //       who: 'bot',
+  //       content: content,
+  //     };
+  //     console.log(conversation);
+  //   } catch (error) {
+  //     let conversation = {
+  //       who: 'bot',
+  //       content: {
+  //         text: {
+  //           text: 'Error가 발생했습니다. 문제를 확인해주세요.',
+  //         },
+  //       },
+  //     };
+  //     console.log(conversation);
+  //   }
+  // };
+
+  // const onKeyHandler = (e) => {
+  //   // 엔터키 이벤트
+  //   if (e.key === 'Enter') {
+  //     // 아무것도 입력하지 않았을 경우
+  //     if (!e.target.value) {
+  //       return alert('메시지를 입력해주세요!');
+  //     }
+  //     // 메시지를 입력했다면 이를 보냄
+  //     textQuery(e.target.value);
+  //     // 보낸 후에는 value를 빈 값으로
+  //     e.target.value = '';
+  //   }
+  // };
 
   return (
     <ChatWrapper>
@@ -187,6 +291,38 @@ function Chatbot() {
             type="text"
             onKeyPress={onKeyHandler}
           />
+          <BotContainer setTimeout={1000}>
+            <BotTitle>Bot</BotTitle>
+            <BotMsg>안녕하세요 저는 등산의 챗봇이에요^0^!</BotMsg>
+          </BotContainer>
+          {sendMsg && (
+            <div>
+              <MyContainer>
+                <MyMsg>{sendMsg}</MyMsg>
+                <MyTitle>Me</MyTitle>
+              </MyContainer>
+              {resMsg && (
+                <BotContainer>
+                  <BotTitle>Bot</BotTitle>
+                  <BotMsg>{resMsg}</BotMsg>
+                </BotContainer>
+              )}
+            </div>
+          )}
+          {secondMsg && (
+            <div>
+              <MyContainer>
+                <MyMsg>{secondMsg}</MyMsg>
+                <MyTitle>Me</MyTitle>
+              </MyContainer>
+            {resSecondMsg && (
+              <BotContainer>
+                <BotTitle>Bot</BotTitle>
+                <BotMsg>{resSecondMsg}</BotMsg>
+              </BotContainer>
+            )}
+          </div>
+          )}
         </ChatContainer>
       )}
     </ChatWrapper>
